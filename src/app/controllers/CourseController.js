eb08b4dbadf87c4ba52688a234a868271c40f675
dotupro <165134392+dotupro@users.noreply.github.com>
2025-05-19 18:03:48 +0700

    add soft delete and delete

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -24,12 +24,12 @@ class CourseController {
 
   // POST /courses/store
   store(req, res, next) {
-    const formData = req.body;
+    const formData = { ...req.body };
     formData.img = `https://img.youtube.com/vi/${req.body.videoId}/0.jpg`;
     const course = new Course(formData);
     course
       .save()
-      .then(() => res.redirect("/"))
+      .then(() => res.redirect("/me/stored/courses"))
       .catch((error) => {
         next(error);
       });
@@ -49,6 +49,27 @@ class CourseController {
   update(req, res, next) {
     Course.updateOne({ _id: req.params.id }, req.body)
       .then(() => res.redirect("/me/stored/courses"))
+      .catch(next);
+  }
+
+  // DELETE /courses/:id
+  delete(req, res, next) {
+    Course.delete({ _id: req.params.id })
+      .then(() => res.redirect("/me/stored/courses"))
+      .catch(next);
+  }
+
+  // DeLETE /courses/:id/force
+  forceDelete(req, res, next) {
+    Course.deleteOne({ _id: req.params.id })
+      .then(() => res.redirect("/me/stored/courses"))
+      .catch(next);
+  }
+
+  // PATCH /courses/:id/restore
+  restore(req, res, next) {
+    Course.restore({ _id: req.params.id })
+      .then(() => res.redirect("/me/trash/courses"))
       .catch(next);
   }
 }
--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 async function connect() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/prider_education_dev", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect("mongodb://localhost:27017/prider_education_dev");
     console.log("Connected to MongoDB successfully!");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
